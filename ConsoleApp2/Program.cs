9b2898d90d598c8c4ee156d813b57920d5c50c5c
Chris King <kingces95@gmail.com>
2021-07-06 10:06:57 -1000

    init

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Graph;
+using Microsoft.Identity.Client;
+using System;
+
+namespace ConsoleApp2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Build a client application.
+			IPublicClientApplication publicClientApplication = PublicClientApplicationBuilder
+						.Create("INSERT-CLIENT-APP-ID")
+						.Build();
+			
+			// Create an authentication provider by passing in a client application and graph scopes.
+			DeviceCodeProvider authProvider = new DeviceCodeProvider(publicClientApplication, graphScopes);
+
+			// Create a new instance of GraphServiceClient with the authentication provider.
+			GraphServiceClient graphClient = new GraphServiceClient(authProvider);
+		}
+	}
+}
